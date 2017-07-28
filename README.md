571cbd09127a8be33c875f5677fcafdc195ea64d
Michael Anderson <manderson@amesbury.com>
2017-07-28 13:36:53 -0500

    Initial code and readme commit

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
 # sqlite
 Browser SQLite
+## Create a Memory Database
+~~~~
+var db = new SQLite();
+var stmt;
+
+db.createStatement('CREATE TABLE states(State char, Ranking int, Population int)).exec();
+
+stmt = db.createStatement('INSERT INTO states(State, Ranking, Population) VALUES(?, ?, ?)');
+
+stmt.exec('California', 1, 38332521);
+stmt.exec('Texas', 2, 26448193);
+stmt.exec('New York', 3, 19651127);
+stmt.exec('Florida', 4, 19552860);
+stmt.exec('Illinois', 5, 12882135);
+stmt.exec('Pennsylvania', 6, 12773801);
+stmt.exec('Ohio', 7, 11570808);
+stmt.exec('Georgia', 8, 9992167);
+stmt.exec('Michigan', 9, 9895622);
+stmt.exec('North Carolina', 10, 9848060);
+stmt.exec('New Jersey', 11, 8899339);
+stmt.exec('Virginia', 12, 8260405);
+stmt.exec('Washington', 13, 6971406);
+stmt.exec('Massachusetts', 14, 6692824);
+stmt.exec('Arizona', 15, 6626624);
+stmt.exec('Indiana', 16, 6570902);
+stmt.exec('Tennessee', 17, 6495978);
+stmt.exec('Missouri', 18, 6044171);
+stmt.exec('Maryland', 19, 5928814);
+stmt.exec('Wisconsin', 20, 5742713);
+stmt.exec('Minnesota', 21, 5420380);
+stmt.exec('Colorado', 22, 5268367);
+stmt.exec('Alabama', 23, 4833722);
+stmt.exec('South Carolina', 24, 4774839);
+stmt.exec('Louisiana', 25, 4625470);
+stmt.exec('Kentucky', 26, 4395295);
+stmt.exec('Oregon', 27, 3930065);
+stmt.exec('Oklahoma', 28, 3850568);
+stmt.exec('Connecticut', 29, 3596080);
+stmt.exec('Iowa', 30, 3090416);
+stmt.exec('Mississippi', 31, 2991207);
+stmt.exec('Arkansas', 32, 2959373);
+stmt.exec('Utah', 33, 2900872);
+stmt.exec('Kansas', 34, 2893957);
+stmt.exec('Nevada', 35, 2790136);
+stmt.exec('New Mexico', 36, 2085287);
+stmt.exec('Nebraska', 37, 1868516);
+stmt.exec('West Virginia', 38, 1854304);
+stmt.exec('Idaho', 39, 1612136);
+stmt.exec('Hawaii', 40, 1404054);
+stmt.exec('Maine', 41, 1328302);
+stmt.exec('New Hampshire', 42, 1323459);
+stmt.exec('Rhode Island', 43, 1051511);
+stmt.exec('Montana', 44, 1015165);
+stmt.exec('Delaware', 45, 925749);
+stmt.exec('South Dakota', 46, 844877);
+stmt.exec('Alaska', 47, 735132);
+stmt.exec('North Dakota', 48, 723393);
+stmt.exec('District of Columbia', 49, 646449);
+stmt.exec('Vermont', 50, 626630);
+stmt.exec('Wyoming', 51, 582658);
+
+~~~~
+
+## Load and Store a Database
+~~~~
+// Load
+var db = new SQLite(localStorage.getItem('myData'));
+
+// ...
+
+// Store
+localStorage.setItem(db.serialize());
+~~~~
---
+++ sqlite.js
@@ -0,0 +1,143 @@
+function SQLite(database) {
+    var connection = this;
+    
+    function loadFromTypedArray(value) {
+        return new SQL.Database(value);
+    }
+    
+    function loadFromArray(value) {
+        return loadFromTypedArray(new Uint8Array(value));
+    }
+    
+    function loadFromString(value) {
+        var re = /[\[\]]/g;
+        return loadFromArray(value.split(',').map(function(s) {
+            s = s.replace(re, '');
+            return parseInt(s);
+        }));
+    }
+    
+    function QuoteString(string) {
+        if (typeof string === 'number') {
+            if (Number.isFinite(string) === false) {
+                return QuoteString(string.toString());
+            }
+            return string.toString();
+        }
+        string = string.toString();
+        string = string.replace(/\\/g, '\\\\');
+        string = string.replace('\x00', '\\x00');
+        string = string.replace('\n', '\\n');
+        string = string.replace('\r', '\\r');
+        string = string.replace("'", "''");
+        return "'" + string + "'";
+    }
+    
+    function Prepare(sql) {
+        // break the sql into chunks of quoted and none quoted
+        // every odd indexed element is quoted
+        var chunks = sql.split("'");
+        var indexes = [];
+        var pos = 0;
+
+        if (chunks.length % 2 === 0) {
+            // if there's an even number of chunks, then the quotes aren't balanced
+            throw new Error("Unbalanced quotes");
+        }
+
+        chunks.map(function(chunk, n) {
+            if (n % 2 === 1) {
+                pos += chunk.length + 2;
+                return;
+            }
+            var subPos = -1;
+            while ((subPos = chunk.indexOf("?", subPos + 1)) >= 0) {
+                indexes.push(pos + subPos);
+            }
+            pos += chunk.length;
+        });
+
+        return {
+            sql: chunks.join("'"),
+            indexes: indexes
+        }
+    }
+    
+    function Join(preparedSql, args) {
+        if (preparedSql.indexes.length !== args.length) {
+            throw new Error("Number of parameters does not match required parameters");
+        }
+
+        var sql = preparedSql.sql;
+        var offset = 0;
+        args.map(function(o, n) {
+            o = QuoteString(o);
+            sql = sql.substr(0, preparedSql.indexes[n] + offset) + o + sql.substr(preparedSql.indexes[n] + offset + 1);
+            offset += o.length - 1;
+        });
+
+        return sql;
+    }
+    
+    function PreparedStatement(sql) {
+        var statement = this;
+        var preparedSQL = Prepare(sql);
+        
+        this.query = function() {
+            var args = Array.prototype.slice.call(arguments);
+            
+            var sqlString = Join(preparedSQL, args);
+            
+            var data = database.exec(sqlString);
+            var result = [];
+            
+            if (data === null || data.length < 1) return [];
+            
+            for (var r = 0; r < data[0].values.length; r++) {
+                var row = {};
+                for (var c = 0; c < data[0].columns.length; c++) {
+                    row[data[0].columns[c]] = data[0].values[r][c];
+                }
+                result.push(row);
+            }
+            return result;
+        }
+        
+        this.exec = function() {
+            var args = Array.prototype.slice.call(arguments);
+            
+            var sqlString = Join(preparedSQL, args);
+            
+            return database.run(sqlString);
+        }
+    }
+    
+    if (typeof database === 'string') {
+        // this is probably a serialized array
+        database = loadFromString(database);
+    } else if (database.__proto__ === Uint8Array.prototype) {
+        database = loadFromTypedArray
+    } else if (database.__proto__ === Uint8Array.prototype) {
+        database = loadFromArray(database)
+    } else if (typeof database === undefined) {
+        database = new SQL.Database();
+    }
+    
+    this.createStatement = function(sql) {
+        return new PreparedStatement(sql);
+    }
+    
+    this.serialize = function() {
+        var data = database.export();
+        var arr = [];
+        
+        for (var n = 0; n < data.length; n++) {
+            arr.push(data[n]);
+        }
+        return JSON.stringify(arr);
+    }
+}
+
+if (typeof SQL === 'undefined') {
+    console.error("sqlite.js requires kripken's sql.js.\r\nAdd <script src='https://cdn.rawgit.com/kripken/sql.js/2dc092a7/js/sql.js'></script> to your page.");
+}
